e0ebb385b0f4500eeb53e8f5784c4bee6e565a8e
Clive <clive@typhon.solutions>
2017-07-24 16:11:53 +0000

    flask-security with registration, email verification and login working. Fixed Flask_mail bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,41 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config.from_envvar('CHIMERA_DEVELOPMENT_SETTINGS')
 
 db = SQLAlchemy(app)
 
+mail = Mail(app)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
 
 @app.route('/')
+@login_required
 def index():
     return 'Flask is running!'
 
