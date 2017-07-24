12fef6450bc65e7a710f62152f5d06c8bf330ee2
Clive <clive@typhon.solutions>
2017-07-24 11:33:30 +0000

    added SQLAlchemy with SQLite connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_envvar('CHIMERA_DEVELOPMENT_SETTINGS')
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
